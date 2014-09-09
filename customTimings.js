d5b18178ab1e282cde7095caa43e5cda43a75bdc
cchi <cchi@ebay.com>
2014-09-09 16:54:00 +0800

    add TTFB info

--- customTimings.js
+++ customTimings.js
@@ -1,21 +1,23 @@
 CustomLoadTiming = window.CustomLoadTiming || {};
 
 CustomLoadTiming.getPerformanceResourceTiming = function(resName) {
-    var array = performance.getEntries();
+    var array = performance.getEntriesByType("resource");
 	
     console.log(resName);
     for(var i =0; i < array.length; i++) {
         var timingObj = array[i];
-	console.log(timingObj);
+	    console.log(timingObj);
         if(timingObj.name.indexOf(resName) > 0) {
-		console.log("Found resource: " + timingObj.name);
-		return timingObj;
+		  console.log("Found resource: " + timingObj.name);
+		  return timingObj;
         }
     }
     return undefined;
 }; 
 
 CustomLoadTiming.fetchUxLoadTimeFromResource = function(resource) {
+    var t = performance.timing;
+    console.log("TTFB: " + (t.responseStart - t.navigationStart));
     var uxTargetPerformanceResourceTiming = CustomLoadTiming.getPerformanceResourceTiming(resource);
     CustomLoadTiming.uxLoadTime = (uxTargetPerformanceResourceTiming) ? uxTargetPerformanceResourceTiming.fetchStart : undefined;
     console.log("Perceived user performance time: " + Math.floor(CustomLoadTiming.uxLoadTime));
